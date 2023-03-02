6ecf5873a10d4cd4ad640473661f2a7293da6d47
Evan <41494790+evaan@users.noreply.github.com>
2023-03-02 02:46:04 -0330

    added potential strafing and turning

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ window.addEventListener('gamepadconnected', (e) => {
     connected.classList = "green";
     debug.textContent = `Index: ${e.gamepad.index}\nId: ${e.gamepad.id}\nButtons: ${e.gamepad.buttons.length}\nAxes: ${e.gamepad.axes.length}`;
     interval = setInterval(() => {
-        info.textContent = `Left Stick: (${round(navigator.getGamepads()[index].axes[0])}, ${round(navigator.getGamepads()[index].axes[1])})
+        var motors = motorCalculation(round(navigator.getGamepads()[index].axes[0]), round(navigator.getGamepads()[index].axes[1]), round(navigator.getGamepads()[index].axes[2]));
+        info.textContent = `Motors:
+        [${motors[0]}, ${motors[1]}]
+        [${-round(navigator.getGamepads()[index].axes[3])}, ${-round(navigator.getGamepads()[index].axes[3])}]
+        [${motors[2]}, ${motors[3]}]
+
+        Left Stick: (${round(navigator.getGamepads()[index].axes[0])}, ${round(navigator.getGamepads()[index].axes[1])})
         Right Stick: (${round(navigator.getGamepads()[index].axes[2])}, ${round(navigator.getGamepads()[index].axes[3])})
         Left Stick Pressed: ${navigator.getGamepads()[index].buttons[10].pressed}
         Right Stick Pressed: ${navigator.getGamepads()[index].buttons[11].pressed}
@@ -25,10 +31,10 @@ window.addEventListener('gamepadconnected', (e) => {
         X Button: ${navigator.getGamepads()[index].buttons[2].pressed}
         Y Button: ${navigator.getGamepads()[index].buttons[3].pressed}
         
-        Up D-Pad ${navigator.getGamepads()[index].buttons[12].pressed}
-        Down D-Pad ${navigator.getGamepads()[index].buttons[13].pressed}
-        Left D-Pad ${navigator.getGamepads()[index].buttons[14].pressed}
-        Right D-Pad ${navigator.getGamepads()[index].buttons[15].pressed}
+        Up D-Pad: ${navigator.getGamepads()[index].buttons[12].pressed}
+        Down D-Pad: ${navigator.getGamepads()[index].buttons[13].pressed}
+        Left D-Pad: ${navigator.getGamepads()[index].buttons[14].pressed}
+        Right D-Pad: ${navigator.getGamepads()[index].buttons[15].pressed}
 
         Left Bumper: ${navigator.getGamepads()[index].buttons[4].pressed}
         Right Bumper: ${navigator.getGamepads()[index].buttons[5].pressed}
@@ -48,5 +54,38 @@ window.addEventListener('gamepaddisconnected', (e) => {
 
 function round(int) {
     if (deadzone > int && -deadzone < int) return 0;
-    else return Math.round(int*100)/100;
+    else return Math.floor(int*100)/100;
+}
+
+function motorCalculation(lx, ly, rx) {
+    ly = -ly; //CHANGE THIS IF THE MOTORS ARE BACKWARDS!!!
+    var frontleft = ly;
+    var frontright = ly;
+    var backleft = ly;
+    var backright = ly;
+    //strafing
+    if (lx > 0) {
+        frontleft -= lx;s
+        frontright += lx;
+        backleft += lx;
+        backright -= lx;
+    } else if (lx < 0) {
+        frontleft += lx;
+        frontright -= lx;
+        backleft -= lx;
+        backright += lx;
+    }
+    //turning
+    if (rx > 0) {
+        frontleft -= rx;
+        frontright -= rx;
+        backleft += rx;
+        backright += rx;
+    } else if (rx < 0) {
+        frontleft += rx;
+        frontright += rx;
+        backleft -= rx;
+        backright -= rx;
+    }
+    return[Math.floor(frontleft*100)/100, Math.floor(frontright*100)/100, Math.floor(backleft*100)/100, Math.floor(backright*100)/100];
 }
